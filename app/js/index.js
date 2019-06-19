f1820354716889b257db526bafff2bc8c1e796fe
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-06-19 17:57:57 +0300

    Header is done

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,17 @@
-"use strict"
+$(document).ready(function(){
+    
+    // toggle header menu
+    $( '.js-toggleMenu' ).click( function() {
+        
+        if ( $( '#menu' ).is( ':hidden' ) ) {
+            $( '#menu' ).show('slideToggle').addClass('isVisible').css('display', 'flex');
+            $(this).addClass('on');
+        } else {
+            $( '#menu' ).hide('slideToggle');
+            $(this).removeClass('on');
+        }
+    });
 
+
+
+});
